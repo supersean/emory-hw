7b242ee4f60f785b2a23ca82b4c26cbcdb862f63
vagrant <vagrant@vagrant.vm>
2019-12-14 01:48:25 +0000

    adding navigation test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,21 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    get root_path
     assert_response :success
   end
+
+  test "should show correct navigation links" do
+    sign_in authors(:one)
+    get root_path
+    assert_select "p" do |elements|
+      puts elements.text.inspect
+    end
+    assert_select "a", { text: "Sign out"}, "Sign out link is not displayed."
+    assert_select "a", { text: "Sign in", count: 0}, "Sign in link displayed."
+    assert_select "a", { text: "Sign up", count: 0}, "Sign up link displayed."
+  end
+
+
 
 end
