57205caf120349ffc7358a99d74c92e4eb8cfe20
vagrant <vagrant@vagrant.vm>
2019-12-14 21:38:53 +0000

    adding authors system tests stubs

---
+++ test/system/authors_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class AuthorsTest < ApplicationSystemTestCase
+  setup do
+    @author = authors(:sean)
+  end
+
+
+  test "visiting index" do
+  
+  end
+
+  test "signing up an author" do
+  end
+
+  test "logging out" do
+  end
+
+  test "logging in" do
+  end
+
+end
